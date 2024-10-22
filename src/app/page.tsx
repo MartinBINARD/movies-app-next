63e5c6ecad4cc8e8b965fd107a5c7283f3eb4a49
MartinBINARD <martin.binard.dev@gmail.com>
2024-10-21 17:31:43 -1000

    Add revalidate home page every 24 hours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import { Genres } from "@/components/genres/Genres";
 import { Popular } from "@/components/Popular/Popular";
 
+const TIME_ELAPSED_IN_ONE_DAY = 86400;
+
+export const revalidate = TIME_ELAPSED_IN_ONE_DAY;
+
 export default function Home() {
   return (
     <div className="font-montserrat flex flex-col pt-2.5 px-12 pb-0 min-h-screen">
