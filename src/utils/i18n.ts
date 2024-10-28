d5c644506ba2aadfc8a3c595a84b7df54f525afe
MartinBINARD <martin.binard.dev@gmail.com>
2024-10-27 17:00:27 -1000

    Add i18n middleware

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,11 +1,26 @@
+import { match } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
+import { NextRequest } from 'next/server';
 
-export const availablesLocales = ['en', 'fr'];
+export const availableLocales = ['en', 'fr'] as const;
 export const defaultLocale = 'fr';
 
-export const getPreferredLocale = (request) => {
-  const headers = { 'accept-language': request.header.get('accept-language') };
-  const languages = new Negotiator();
+export const getPreferredLocale = (request: NextRequest): string => {
+  const headers = { 'accept-language': request.headers.get('accept-language') || '' };
+  const languages = new Negotiator({ headers }).languages();
+
+  return match(languages, availableLocales, defaultLocale);
 };
 
-export const getLocaleUrlToRedirect = (request) => {};
+export const getLocaleUrlToRedirect = (request: NextRequest): URL | null => {
+  const pathname = new URL(request.url).pathname;
+  const pathnameIsMissingLocale = availableLocales.every((locale) => !pathname.startsWith(`/${locale}`) && pathname !== `/${locale}`);
+
+  if (pathnameIsMissingLocale) {
+    const locale = getPreferredLocale(request);
+
+    return new URL(`/${locale}${pathname}`, request.url);
+  }
+
+  return null;
+};
